5d6780c34694ff909cb39332be743b1587049120
SorahISA <willy097212@gmail.com>
2023-10-15 02:29:04 +0800

    removed default code

--- codebook/1_Basic/readchar.cpp
+++ codebook/1_Basic/readchar.cpp
@@ -1,7 +1,15 @@
-inline char readchar() {
-  static const size_t bufsize = 65536;
-  static char buf[bufsize];
-  static char *p = buf, *end = buf;
-  if (p == end) end = buf + fread_unlocked(buf, 1, bufsize, stdin), p = buf;
-  return *p++;
+#include <unistd.h>
+const int S = 65536;
+inline char RC() {
+  static char buf[S], *p = buf, *q = buf;
+  return p == q and (q = (p = buf) + read(0, buf, S)) == buf ? -1 : *p++;
+}
+inline int RI() {
+  static char c; int a;
+  while (((c = RC()) < '0' or c > '9') and c != '-' and c != -1);
+  if (c == '-') { a = 0;
+    while ((c = RC()) >= '0' and c <= '9') a *= 10, a -= c ^ '0'; }
+  else { a = c ^ '0';
+    while ((c = RC()) >= '0' and c <= '9') a *= 10, a += c ^ '0'; }
+  return a;
 }
