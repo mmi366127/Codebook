837af481f9cd7beb9d26216fc52dc5074436ad77
coldEr66 <seal1000402@gmail.com>
2023-10-21 11:34:14 +0800

    Characteristic Polynomial template type T

--- codebook/6_Math/Characteristic_Polynomial.cpp
+++ codebook/6_Math/Characteristic_Polynomial.cpp
@@ -1,38 +1,39 @@
-void hessenberg_reduction(vector<vector<int>> &M) {
+template <class Tp> void hessenberg_reduction(vector<vector<Tp>> &M) {
   // assert(M.size() == M[0].size());
   const int N = M.size();
-  for (int r = 0; r < N-2; r++) {
+  for (int r = 0; r < N - 2; r++) {
     int piv = -1;
-    for (int h = r+1; h < N; ++h) {
-      if (M[h][r] != 0) {piv = h; break;}
+    for (int h = r + 1; h < N; ++h) {
+      if (M[h][r] != 0) {
+        piv = h;
+        break;
+      }
     }
     if (piv < 0) continue;
-    for (int i = 0; i < N; i++) swap(M[r+1][i], M[piv][i]);
-    for (int i = 0; i < N; i++) swap(M[i][r+1], M[i][piv]);
-    
-    const int rinv = fpow(M[r+1][r]);
-    for (int i = r+2; i < N; i++) {
-        const auto n = M[i][r] * rinv % mod;
-        for (int j = 0; j < N; j++) M[i][j] = (M[i][j] - M[r+1][j] * n % mod + mod) % mod;
-        for (int j = 0; j < N; j++) M[j][r+1] = (M[j][r+1] + M[j][i] * n % mod) % mod;
+    for (int i = 0; i < N; i++) swap(M[r + 1][i], M[piv][i]);
+    for (int i = 0; i < N; i++) swap(M[i][r + 1], M[i][piv]);
+    const auto rinv = Tp(1) / M[r + 1][r];
+    for (int i = r + 2; i < N; i++) {
+      const auto n = M[i][r] * rinv;
+      for (int j = 0; j < N; j++) M[i][j] -= M[r + 1][j] * n;
+      for (int j = 0; j < N; j++) M[j][r + 1] += M[j][i] * n;
     }
   }
 }
-
-vector<int> characteristic_poly(vector<vector<int>> M) {
+template <class Tp> vector<Tp> characteristic_poly(vector<vector<Tp>> M) {
   hessenberg_reduction(M);
   const int N = M.size();
-  vector<vector<int>> p(N+1);
+  vector<vector<Tp>> p(N + 1);
   p[0] = {1};
-  for (int i=0; i<N; i++) {
-    p[i+1].assign(i+2, 0);
-    for (int j = 0; j < i+1; j++) p[i+1][j+1] = (p[i+1][j+1] + p[i][j]) % mod;
-    for (int j = 0; j < i+1; j++) p[i+1][j] = (p[i+1][j] - p[i][j] * M[i][i] % mod + mod) % mod;
-    int betas = 1;
-    for (int j = i-1; j >= 0; j--) {
-      betas = betas * M[j+1][j] % mod;
-      int hb = (mod - M[j][i]) * betas % mod;
-      for (int k = 0; k < j+1; k++) p[i+1][k] = (p[i+1][k] + hb * p[j][k] % mod) % mod;
+  for (int i = 0; i < N; i++) {
+    p[i + 1].assign(i + 2, 0);
+    for (int j = 0; j < i + 1; j++) p[i + 1][j + 1] += p[i][j];
+    for (int j = 0; j < i + 1; j++) p[i + 1][j] -= p[i][j] * M[i][i];
+    Tp betas = 1;
+    for (int j = i - 1; j >= 0; j--) {
+      betas *= M[j + 1][j];
+      Tp hb = -M[j][i] * betas;
+      for (int k = 0; k < j + 1; k++) p[i + 1][k] += hb * p[j][k];
     }
   }
   return p[N];
