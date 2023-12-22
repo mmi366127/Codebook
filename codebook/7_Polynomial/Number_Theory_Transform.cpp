687a66731cadf225f87a66209715b98f63811d6d
coldEr66 <seal1000402@gmail.com>
2023-12-22 16:22:38 +0800

    NTT update

--- codebook/7_Polynomial/Number_Theory_Transform.cpp
+++ codebook/7_Polynomial/Number_Theory_Transform.cpp
@@ -2,28 +2,29 @@
 //7*17*(2^23)+1, 998244353, 3
 //1255*(2^20)+1, 1315962881, 3
 //51*(2^25)+1, 1711276033, 29
-template<int maxn, int P, int RT> //maxn must be 2^k
+template<int P, int RT> //maxn must be 2^k
 struct NTT {
-  int w[maxn];
-  int mpow(int a, int n);
-  int minv(int a) { return mpow(a, P - 2); }
-  NTT() {
+  int maxn;
+  vector<int> w;
+  NTT(int n) {
+    maxn = 2 << __lg(n+1);
+    w.resize(maxn);
     int dw = mpow(RT, (P - 1) / maxn);
     w[0] = 1;
     for (int i = 1; i < maxn; ++i) w[i] = w[i - 1] * dw % P;
   }
-  void bitrev(int *a, int n) {
+  void bitrev(vector<int> &a) {
     int i = 0;
-    for (int j = 1; j < n - 1; ++j) {
-      for (int k = n >> 1; (i ^= k) < k; k >>= 1);
+    for (int j = 1; j < maxn - 1; ++j) {
+      for (int k = maxn >> 1; (i ^= k) < k; k >>= 1);
       if (j < i) swap(a[i], a[j]);
     }
   }
-  void operator()(int *a, int n, bool inv = false) { //0 <= a[i] < P
-    bitrev(a, n);
-    for (int L = 2; L <= n; L <<= 1) {
+  void operator()(vector<int> &a, bool inv = false) { //0 <= a[i] < P
+    bitrev(a);
+    for (int L = 2; L <= maxn; L <<= 1) {
       int dx = maxn / L, dl = L >> 1;
-      for (int i = 0; i < n; i += L) {
+      for (int i = 0; i < maxn; i += L) {
         for (int j = i, x = 0; j < i + dl; ++j, x += dx) {
           int tmp = a[j + dl] * w[x] % P;
           if ((a[j + dl] = a[j] - tmp) < 0) a[j + dl] += P;
@@ -32,9 +33,9 @@ struct NTT {
       }
     }
     if (inv) {
-      reverse(a + 1, a + n);
-      int invn = minv(n);
-      for (int i = 0; i < n; ++i) a[i] = a[i] * invn % P;
+      reverse(1 + begin(a), end(a));
+      int invn = fpow(maxn, P-2); // do fpow
+      for (int i = 0; i < maxn; ++i) a[i] = a[i] * invn % P;
     }
   }
 };
